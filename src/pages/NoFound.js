2172f04407a5d6fad7761acaacf584af496dd167
sambeck87 <sandro_mipb@hotmail.com>
2023-02-26 21:04:51 -0600

    Fix linter errors

--- src/pages/NoFound.js
+++ src/pages/NoFound.js
@@ -5,7 +5,44 @@ import '../styles/notFound.css';
 const NoFound = () => (
   <div id="notFound" className="flex column">
     <p id="notFoundMesagge" className="center">
-      Querido gato loco, creo que erraste el camino. Regresa a la página de inicio.
+      SÓLO EN CASO DE QUE ALGÚN GATO LOCO,
+      ALGÚN DÍA, ENCUENTRA ESTA NOTA
+      ¡OJALÁ QUE NUNCA OCURRA!
+
+      No tengo claro por dónde comenzar…
+      Resultaría sobremanera sencillo hacerlo
+      respondiendo a la manida duda
+      existencial ¿quién soy? Si tan sólo lo
+      supiera, pero incluso de ello he
+      comenzado a dudar estando aquí…
+      Resultaría igual de sencillo hacerlo
+      respondiendo a la pregunta tan
+      recurrente en el ámbito teatral ¿en
+      dónde estoy? Del mismo modo, si tan
+      sólo poseyera dicha información, pero
+      ni mapa, ni brújula, ni sistema de
+      posicionamiento han sido capaces de
+      brindar datos útiles… Pareciera, por
+      disparatado que suene, que he dejado de
+      existir o, cuando menos, que alcancé la
+      frontera entre lo existente y lo
+      inexistente; de otro modo no logro
+      explicar este blanco infinito
+      devorándolo todo.
+
+      ¡NaH te creas, sólo has encontrado el
+      infausto error 404 de esta nave de los
+      gatos locos! Desconozco qué estabas
+      buscando, quizá giraste a la derecha
+      cuando debías hacerlo a la izquierda,
+      quizá giraste a la izquierda cuando
+      debías hacerlo a la derecha, o quizá
+      seguiste de largo cuando debías girar…
+      Haya sido como haya sido, en alguna de
+      las encrucijadas, erraste el camino.
+
+      Atentamente:
+      Damián Sastré
     </p>
     <div id="image" />
   </div>
